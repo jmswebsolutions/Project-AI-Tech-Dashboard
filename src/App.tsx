677c50a3b1508d9a0fc858b3995e191451a98389
Jonatan Marques <jonatanmarsv@gmail.com>
2026-06-10 16:30:02 -0300

    atualização

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import './styles/index.css';
 
